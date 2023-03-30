5b102e594d40b70563150151d1238a6391211f3a
MatiushenkoOleksandr <126600935+MatiushenkoOleksandr@users.noreply.github.com>
2023-03-30 17:46:22 +0300

    home work lesson 8

---
+++ library/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public Biography Biography { get; set; }
+        private List<Book> Books { get; set; }
+        private int Id { get; set; }
+
+       
+
+
+        
+        public Author (List<Book> books)
+        {
+            Books = books;
+        }
+    }
+}
