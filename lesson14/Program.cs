91f6529f85a90800d31a56f6d8318300d9e0f94e
MatiushenkoOleksandr <126600935+MatiushenkoOleksandr@users.noreply.github.com>
2023-04-12 18:01:15 +0300

    homework

---
+++ lesson14/Program.cs
@@ -0,0 +1,12 @@
+﻿using lesson14;
+
+var arr = new CustomStack<int>();
+arr.Push(2);
+arr.Push(1);
+arr.Push(9);
+arr.Push(6);
+arr.Push(3);
+arr.Peek();
+    arr.Pop();
+arr.Clear();
+var a = 0;
---
+++ lesson14/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson14
+{
+    public class CustomStack<T>
+
+    {
+        public CustomStack()
+        {
+            Values = new T[0];
+        }
+        private T[] Values;
+
+        public void Push(T value)
+        {
+            var valueList = Values.ToList();
+
+            valueList.Insert(0, value);
+
+            Values=valueList.ToArray();
+        }
+        public T Pop()
+        {
+            var newValueList = Values.ToList();
+            var a = newValueList[newValueList.Count - 1];
+            newValueList.Remove(a);
+            Values=newValueList.ToArray();
+            return a;
+        }
+        public void Clear()
+        {
+            Array.Resize(ref Values, 0);
+
+        }
+        public int Count { get => Values.Length; }
+
+        public T Peek()
+        {
+            return Values[0];
+        }
+
+        public void CopyTo(ref T[] array)
+        {
+
+            Array.Copy(Values, array, Values.Length);
+        }
+
+    }
+}
