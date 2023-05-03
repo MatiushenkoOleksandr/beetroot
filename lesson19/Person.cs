0f325c17edc3c2da77e2bac1868a9ea9154b112b
MatiushenkoOleksandr <126600935+MatiushenkoOleksandr@users.noreply.github.com>
2023-05-03 17:19:45 +0300

    homework LINQ

---
+++ lesson19/Person.cs
@@ -0,0 +1,109 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson19
+{
+    public enum Gender
+    {
+        male,
+        female,
+    }
+
+    public class Friend
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Person
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("index")]
+        public int Index { get; set; }
+
+        [JsonProperty("guid")]
+        public Guid GuidId { get; set; }
+
+        [JsonProperty("isActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty("balance")]
+        public string Balance { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+
+        [JsonProperty("eyeColor")]
+        public string EyeColor { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("company")]
+        public string Company { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("about")]
+        public string About { get; set; }
+
+        [JsonProperty("registered")]
+        public string Registered { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
+
+        [JsonProperty("friends")]
+        public List<Friend> Friends { get; set; }
+
+        public double GetDistance(Person person)
+        {
+            var distanceX = Longitude - person.Longitude;
+            var distanceY = Latitude - person.Latitude;
+            var distance = distanceX * distanceX + distanceY * distanceY;
+            distance = Math.Sqrt(distance);
+
+            return distance;
+        }
+
+        public int GetCountOfSameWords(Person person)
+        {
+            var words = About.Split(" ");
+            var words2 = person.About.Split(" ");
+            var count = words.Where(a=> words2.Any(b=> a.Contains(b))).Count();
+            return count;
+
+        }
+        public int GetSameFriends(Person person)
+        {
+            var names = Friends.Select(a => a.Name.Split(" ")[0]).Intersect(person.Friends.Select(b => b.Name.Split(" ")[0])).ToList();
+            return names.Count;
+        }
+
+    }
+}
---
+++ lesson19/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using lesson19;
+using Newtonsoft.Json;
+
+var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("D:\\Git\\repository\\repo\\lesson19\\People.json")).ToList();
+var person1 = persons.Max(a => a.Latitude);
+var person2 = persons.Min(a => a.Latitude);
+var person3 = persons.Max(b => b.Longitude);
+var person4 = persons.Min(b => b.Longitude);
+var minDistance = persons[0].GetDistance(persons[1]);
+var maxDistance = persons[0].GetDistance(persons[1]);
+
+for (int i = 0; i < persons.Count - 1; i++)
+{
+    for (int j = i + 1; j < persons.Count; j++)
+    {
+        var distance = persons[i].GetDistance(persons[j]);
+        if (distance < minDistance)
+        {
+            minDistance = distance;
+        }
+        if (distance > maxDistance)
+        {
+            maxDistance = distance;
+        }
+
+    }
+}
+
+var twoPersonsWithSameWords = persons.SelectMany(r => persons.Where(m => m.Id != r.Id).Select(p => p.GetCountOfSameWords(r)));
+var firstPersonWithSameFriend = new Person();
+var secondPersonWithSameFriend = new Person();
+
+for (int i = 0; i < persons.Count; i++)
+{
+    var sameFriendPerson = persons.FirstOrDefault(a => a.Id != persons[i].Id && a.GetSameFriends(persons[i]) > 0);
+   firstPersonWithSameFriend= sameFriendPerson;
+    secondPersonWithSameFriend = persons[i];
+    if (sameFriendPerson != null)
+    {
+        break;
+    } 
+}
+
+//var m = persons.Select(p => persons.Where(m => m.Id != p.Id && p.GetSameFriends(m) > 0).ToList()).ToList();
+
+//var r = persons.SelectMany(r => persons.Where(m => m.Id != r.Id).Select(p => p.GetDistance(r))).Min();
+//var b = persons.SelectMany(r => persons.Where(m => m.Id != r.Id).Select(p => p.GetDistance(r))).Max();
+//var newList = persons.Select(a => a.GetDistance(persons[5])).ToList();
